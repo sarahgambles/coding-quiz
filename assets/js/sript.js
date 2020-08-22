7d68296ade6cf0192f5b10b9ed3218356b5241ab
Sarah Gambles <sarahgambles14@gmail.com>
2020-08-22 11:25:08 -0600

    added variable names

---
+++ assets/js/sript.js
@@ -0,0 +1,11 @@
+// variables with query selectors
+var timerEl = document.getElementById('countdown');
+var startBtn = document.getElementById('start');
+var count = 0;
+var incrementEl = document.querySelector("#increment");
+var decrementEl = document.querySelector ("#decrement");
+var countEl = document.querySelector("#count");
+var initialsInput = document.querySelector("#initials");
+var scoreInput = document.querySelector("#score");
+var questions = document.querySelector();
+var answer =
