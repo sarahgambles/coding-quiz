6b381a3417141b084515e47fd82fb2910f50336d
Sarah Gambles <sarahgambles14@gmail.com>
2020-08-22 11:28:25 -0600

    added timing function

--- assets/js/sript.js
+++ assets/js/sript.js
@@ -9,3 +9,26 @@ var initialsInput = document.querySelector("#initials");
 var scoreInput = document.querySelector("#score");
 var questions = document.querySelector();
 var answer =
+
+// click the start button with an array
+ 
+// add event function
+ 
+// timer starts
+function countdown() {
+    var timeLeft = 30;
+  
+    // setInterval to call a function to execute a new question
+    var timeInterval = setInterval(function() {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining ';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining ';
+            timeLeft--;
+        } else {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+ 
